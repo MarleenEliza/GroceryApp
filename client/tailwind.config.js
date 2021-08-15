f25c8beda6cf6725278e6357036d64078fc07836
MarleenEliza <62828450+MarleenEliza@users.noreply.github.com>
2021-08-16 00:12:42 +0900

    add tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      green: {
+        light: "#eef9e7",
+        DEFAULT: "#bce08a",
+      },
+      yellow: {
+        light: "#f9f8e7",
+        DEFAULT: "#e0cf8a",
+      },
+      red: {
+        light: "#f9e7e7",
+        DEFAULT: "#e08a8a",
+      },
+      gray: {
+        light: "#d1c8c8",
+        DEFAULT: "#5c5c5c",
+      },
+      white: "#ffffff",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
