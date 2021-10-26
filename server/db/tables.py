49b82e0205991b022f5855e670b9e77c64260ee5
MarleenEliza <62828450+MarleenEliza@users.noreply.github.com>
2021-10-26 21:59:02 +0900

    fix path and env settings

--- server/db/tables.py
+++ server/db/tables.py
@@ -1,6 +1,9 @@
 import enum
+
+
 class view_tables(enum.Enum):
-   super_market_view = "super_market_view" 
-   recipe_view = "recipe_view"
+    super_market_view = "super_market_view"
+    recipe_view = "recipe_view"
 
 # TODO: enums for normal tables
+# TODO: look at SQL light
